a2a20d15b4c55ba3727a90b7059c4ea4d675cbbd
hopperelec <hopperelec@gmail.com>
2024-02-03 17:54:27 +0000

    Explicit typing of layout props

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -2,10 +2,12 @@ import type { LayoutServerLoad } from "./$types";
 import getPlayerFor from "$lib/get-player-for";
 import prisma from "$lib/prisma";
 import { error } from "@sveltejs/kit";
+import type { LocalDoor } from "$lib/types";
+import type { Player } from "@prisma/client";
 
 export const load: LayoutServerLoad = async ({ url, locals }) => {
   const player = await getPlayerFor(locals.user, url);
-  const playerData = await prisma.player.findUnique({
+  const ret = await prisma.player.findUnique({
     where: {
       id: player.id,
     },
@@ -19,15 +21,22 @@ export const load: LayoutServerLoad = async ({ url, locals }) => {
       },
     },
   });
-  if (!playerData)
+  if (!ret)
     throw error(
       500,
       "An unexpected error occurred while trying to retrieve your player data",
     );
-  return {
+  const { game, ...playerData } = ret; // So that playerData doesn't contain duplicate data from game
+  const props: {
+    picture: string;
+    player: Player;
+    mapData: string;
+    doors: LocalDoor[];
+  } = {
     picture: locals.user.picture,
     player: playerData,
-    mapData: await (await fetch(playerData.game.map.imgURL)).text(),
-    doors: playerData.game.map.doors,
+    mapData: await (await fetch(game.map.imgURL)).text(),
+    doors: game.map.doors,
   };
+  return props;
 };
