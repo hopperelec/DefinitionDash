c8f28b21f70c1b77f1d6f7c5955475e9dab49ae6
hopperelec <hopperelec@gmail.com>
2024-02-11 23:50:06 +0000

    Enabled Vercel's ISR for `/maps/[mapId]/doors/`

--- src/routes/maps/[mapId=id]/doors/+server.ts
+++ src/routes/maps/[mapId=id]/doors/+server.ts
@@ -1,6 +1,13 @@
 import prisma from "$lib/server/prisma";
 import { error } from "@sveltejs/kit";
 import encodeDoors from "$lib/server/encode-doors";
+import type { Config } from "@sveltejs/adapter-vercel";
+
+export const config: Config = {
+  isr: {
+    expiration: 60,
+  },
+};
 
 export const GET = async ({ params, locals }) => {
   const map = await prisma.map.findFirst({
