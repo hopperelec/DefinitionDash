250723e603ffe8b4cda6185f34ac1e6fb66005d5
hopperelec <hopperelec@gmail.com>
2024-02-26 15:50:40 +0000

    Refactor doors mapper to get room centers in `drawLine`

--- src/routes/teacher/maps/[mapId=id]/doors/+page.svelte
+++ src/routes/teacher/maps/[mapId=id]/doors/+page.svelte
@@ -1,57 +1,48 @@
 <script lang="ts">
   import SVGMap from "$lib/SVGMap.svelte";
   import { SVG_NS } from "$lib/constants";
-  import type { LocalDoor } from "$lib/types";
   import decodeDoors from "$lib/decode-doors";
 
   export let data;
   const lines: { [key: number]: { [key: number]: SVGLineElement } } = {}; // First key is room1Id, second key is room2Id
   let map: SVGMap;
-  let firstRoom: number;
-  let firstRoomCenter: DOMPoint | undefined;
+  let _firstRoom: number = 0;
 
-  function drawLine(
-    door: LocalDoor,
-    firstRoomCenter?: DOMPoint,
-    secondRoomCenter?: DOMPoint,
-  ) {
+  function drawLine(svgRef1: number, svgRef2: number) {
+    const firstRoomCenter = map.getCenterOf(svgRef1);
+    const secondRoomCenter = map.getCenterOf(svgRef2);
     if (firstRoomCenter && secondRoomCenter) {
-      const line = map
-        .getSVG()
+      const line = map.getSVG()
         .appendChild(document.createElementNS(SVG_NS, "line"));
       line.setAttribute("x1", firstRoomCenter.x.toString());
       line.setAttribute("y1", firstRoomCenter.y.toString());
       line.setAttribute("x2", secondRoomCenter.x.toString());
       line.setAttribute("y2", secondRoomCenter.y.toString());
       line.classList.add("door-line");
-      if (!lines[door.svgRef1]) lines[door.svgRef1] = {};
-      lines[door.svgRef1][door.svgRef2] = line;
+      if (!lines[svgRef1]) lines[svgRef1] = {};
+      lines[svgRef1][svgRef2] = line;
     }
   }
 
   async function onClickRoom(clickedRoom: number) {
-    if (!firstRoomCenter) {
-      firstRoom = clickedRoom;
-      firstRoomCenter = map.getCenterOf(clickedRoom);
+    if (!_firstRoom) {
+      _firstRoom = clickedRoom;
     } else {
-      const secondRoomCenter = map.getCenterOf(clickedRoom);
-      const door = {
-        svgRef1: Math.min(firstRoom, clickedRoom),
-        svgRef2: Math.max(firstRoom, clickedRoom),
-      };
-      if (lines[door.svgRef1] && lines[door.svgRef1][door.svgRef2]) {
-        lines[door.svgRef1][door.svgRef2].remove();
-        delete lines[door.svgRef1][door.svgRef2];
-        await fetch(door.svgRef1 + "/" + door.svgRef2, {
+      const svgRef1 = Math.min(_firstRoom, clickedRoom);
+      const svgRef2 = Math.max(_firstRoom, clickedRoom);
+      if (lines[svgRef1] && lines[svgRef1][svgRef2]) {
+        lines[svgRef1][svgRef2].remove();
+        delete lines[svgRef1][svgRef2];
+        await fetch(svgRef1 + "/" + svgRef2, {
           method: "DELETE",
         });
-      } else if (clickedRoom !== firstRoom) {
-        drawLine(door, firstRoomCenter, secondRoomCenter);
-        await fetch(door.svgRef1 + "/" + door.svgRef2, {
+      } else if (clickedRoom !== _firstRoom) {
+        drawLine(svgRef1, svgRef2);
+        await fetch(svgRef1 + "/" + svgRef2, {
           method: "PUT",
         });
       }
-      firstRoomCenter = undefined;
+      _firstRoom = 0;
     }
   }
 
@@ -61,8 +52,7 @@
       .then(decodeDoors);
     for (const [svgRef1, svgRef2s] of Object.entries(doors)) {
       for (const svgRef2 of svgRef2s) {
-        const door = { svgRef1: +svgRef1, svgRef2 };
-        drawLine(door, map.getCenterOf(door.svgRef1), map.getCenterOf(svgRef2));
+        drawLine(+svgRef1, svgRef2);
       }
     }
   }
