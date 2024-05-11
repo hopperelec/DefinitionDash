dcd4b0ca04e11b8d80e98b9141686735d6d7797b
hopperelec <hopperelec@gmail.com>
2024-05-11 11:13:44 +0100

    Added comments to start endpoint

--- src/routes/game/[gameId=id]/lobby/start/+server.ts
+++ src/routes/game/[gameId=id]/lobby/start/+server.ts
@@ -3,10 +3,12 @@ import { error, redirect } from "@sveltejs/kit";
 import ablyServer from "$lib/server/ably-server";
 
 async function startGame(gameId: number) {
+  // Start the game in the database
   await prisma.game.update({
     where: { id: gameId },
     data: { state: "ONGOING" },
   });
+  // Start the game in realtime
   ablyServer.channels
     .get("game:" + gameId + ":lobby")
     .publish("start", null)
@@ -14,6 +16,7 @@ async function startGame(gameId: number) {
 }
 
 export const GET = async ({ params, locals }) => {
+  // Ensure the player is allowed to start the game
   const gameId = +params.gameId;
   const player = await prisma.player.findUnique({
     where: { userId_gameId: { userId: locals.user.id, gameId } },
@@ -26,6 +29,8 @@ export const GET = async ({ params, locals }) => {
   if (player.game.state != "LOBBY")
     error(403, "This game has already started!");
   if (!player.isHost) error(403, "Only the game's host can start it!");
+
   await startGame(gameId);
+  // Redirect the host to the started game
   redirect(301, "/game/" + gameId + "/");
 };
