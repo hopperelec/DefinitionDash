5ef7628451248d2e1d717d8dee935c13d3950d89
hopperelec <hopperelec@gmail.com>
2024-05-11 11:11:56 +0100

    Added comments to leave endpoint

--- src/routes/game/[gameId=id]/lobby/leave/+server.ts
+++ src/routes/game/[gameId=id]/lobby/leave/+server.ts
@@ -3,10 +3,12 @@ import { error, redirect } from "@sveltejs/kit";
 import ablyServer from "$lib/server/ably-server";
 
 async function assignHost(gameId: number, userId: number) {
+  // Assign as a host in the database
   await prisma.player.update({
     where: { userId_gameId: { userId, gameId } },
     data: { isHost: true },
   });
+  // Assign as a host in realtime
   ablyServer.channels
     .get("game:" + gameId + ":lobby")
     .publish("host", { userId })
@@ -36,17 +38,20 @@ export const GET = async ({ params, locals }) => {
   if (!player) error(403, "You are not in this game!");
   if (player.game.state != "LOBBY")
     error(403, "You can only leave a game whilst it is in the lobby!");
+  // Remove player in the database
   await prisma.player.delete({
     where: { userId_gameId: { userId: locals.user.id, gameId } },
   });
+  // Remove player in realtime
   ablyServer.channels
     .get("game:" + gameId + ":lobby")
     .publish("leave", { userId: locals.user.id })
     .then();
   if (
-    player.game.players.length != 0 &&
-    !player.game.players.some((player) => player.isHost)
+    player.game.players.length != 0 && // there are other players
+    !player.game.players.some((player) => player.isHost) // none of those players are a host
   )
     await assignHost(gameId, player.game.players[0].userId);
+  // Redirect to home page
   redirect(302, "/");
 };
