8be3d23997109c96aa1c210f9875f4a113845da1
hopperelec <hopperelec@gmail.com>
2024-05-11 09:25:46 +0100

    Added comments to end page

--- src/routes/game/[gameId=id]/end/+page.svelte
+++ src/routes/game/[gameId=id]/end/+page.svelte
@@ -8,10 +8,9 @@
   export let data;
   let map: SVGMap;
 
+  // won, came 1st, came 2nd, came 3rd, came 4th...
   function getCardinal(position: number) {
-    if (position === 1) {
-      return "won";
-    }
+    if (position === 1) return "won";
     const lastDigit = position % 10;
     const lastTwoDigits = position % 100;
     let suffix = "th";
@@ -26,6 +25,7 @@
   }
 
   function onMapSuccess() {
+    // Add player icon for all players
     for (const player of data.players) {
       const icon = map.addIconTo(
         player.currSvgRef,
@@ -36,6 +36,7 @@
       }
     }
 
+    // Add point icon to all unclaimed rooms
     for (const roomElm of map.getSVG().querySelectorAll("[data-room]")) {
       const svgRef = (roomElm as HTMLElement).dataset.room;
       if (svgRef && !data.claimedRooms.includes(+svgRef)) {
